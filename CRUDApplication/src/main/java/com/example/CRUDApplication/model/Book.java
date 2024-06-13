38070f7859b453d4862e2788a424b9355fad496d
Arretados Labs <arretadolabs@gmail.com>
2024-06-13 14:51:41 -0300

    Inserting lines of comments explaining all annotations used in class

--- CRUDApplication/src/main/java/com/example/CRUDApplication/model/Book.java
+++ CRUDApplication/src/main/java/com/example/CRUDApplication/model/Book.java
@@ -3,13 +3,16 @@ package com.example.CRUDApplication.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name="Books")
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
-@Getter
-@ToString
+/***
+ * Section of annotation and you explain
+ */
+@Entity //Used for information and identify class used also entity
+@Table(name="Books") //For identify and create with hibernate link a table called "Books"
+@NoArgsConstructor // Create constructor default no arguments
+@AllArgsConstructor // Create constructor with all arguments
+@Setter // Generate Setters for update values in attribute
+@Getter // Generate Getters for return value of attributes
+@ToString // Generate method ToString for converter data in format String
 public class Book {
 
     @Id
